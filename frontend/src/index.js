98db5cc48b109752e1dd3f7abfe1a42dd95a05b5
Nickolay Dudaryk <nick.dudaryk@gmail.com>
2021-07-18 18:37:50 +0300

    separate frontend and backend folders

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-import "../src/styles/_global.scss";
+import "./styles/_global.scss";
 
 ReactDOM.render(
   <>
